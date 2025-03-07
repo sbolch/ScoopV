bd05fa86b7df1f02c32103e88a53cf2e9ed1b933
Szabolcs Surányi <sbolch@pm.me>
2025-03-07 11:47:21 +0100

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "sbolch/ScoopV:master"
+    [String]$upstream = "sbolch/ScoopV:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
